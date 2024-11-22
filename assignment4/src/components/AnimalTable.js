695ceb5a65f41180dd90b1df7347b5711ace08ee
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-11-22 10:48:14 +0000

    update animalTable.js file

--- assignment4/src/components/AnimalTable.js
+++ assignment4/src/components/AnimalTable.js
@@ -1,10 +1,10 @@
-function AnimalTable(props){
-    return(
-        <div className = 'gameContainer'>
-            <table className = 'table'>
+function AnimalTable(props) {
+    return (
+        <div className='gameContainer'>
+            <table className='table'>
                 <thead>
                     <tr>
-                        <th colspan = "3">
+                        <th colspan="3">
                             <h2>ANIMAL MATCHING GAME</h2>
                         </th>
                     </tr>
@@ -16,13 +16,29 @@ function AnimalTable(props){
                 </thead>
                 <tbody>
                     <tr>
-                        <td className = 'result-col' width = "10%">
+                        <td className='result-col' width="10%">
                             <h2>{result}</h2>
                         </td>
-                        
+
+                        <td className='animalname-col' width="20%">
+                            <h2>{randomAnimal.name.toUpperCase()}</h2>
+                        </td>
+                        <td classname='animalgrid-col' width="70%">
+                            <div>
+                                <div className='animalgrid'>
+                                    {animals.map((animal) => (
+                                        <div key={animal.name} className='animalgrid-item' onClick={() => handleAnimalClick(animal.name)}>
+                                            <img src={require(`../assets/img/${animal.img}`)} alt={animal.name} className='animal-image' />
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </div>
     )
 }
+
+export default AnimalTable
