b381742194ee210c1756eddce65b7dd729ccae0d
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-11-22 10:27:05 +0000

    create the outline of the table

---
+++ assignment4/src/components/AnimalTable.js
@@ -0,0 +1,15 @@
+function AnimalTable(props){
+    return(
+        <div className = 'gameContainer'>
+            <table className = 'table'>
+                <thead>
+                    <tr>
+                        <th>
+                            
+                        </th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    )
+}
