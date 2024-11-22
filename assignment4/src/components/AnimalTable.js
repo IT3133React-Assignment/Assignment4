50cfe445b05f2a4c52f5c2efa746c284a9ebcecb
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-11-22 10:37:35 +0000

    update AnimalTable.js file

--- assignment4/src/components/AnimalTable.js
+++ assignment4/src/components/AnimalTable.js
@@ -4,11 +4,24 @@ function AnimalTable(props){
             <table className = 'table'>
                 <thead>
                     <tr>
-                        <th>
-                            
+                        <th colspan = "3">
+                            <h2>ANIMAL MATCHING GAME</h2>
                         </th>
                     </tr>
+                    <tr>
+                        <th><h3>Result</h3></th>
+                        <th><h3>Animal Name</h3></th>
+                        <th><h3>Select the Animal</h3></th>
+                    </tr>
                 </thead>
+                <tbody>
+                    <tr>
+                        <td className = 'result-col' width = "10%">
+                            <h2>{result}</h2>
+                        </td>
+                        
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
